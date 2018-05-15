acadc34bbb5033acee0201935c8f933c913135ab
Alexander Gehling <alexander.gehling@gmail.com>
2018-05-15 17:00:12 +0200

    Made it so that creating a group adds you to the group.

--- app/src/main/java/com/example/mathias/findyourfriends/Database/DatabaseConnector.java
+++ app/src/main/java/com/example/mathias/findyourfriends/Database/DatabaseConnector.java
@@ -7,8 +7,11 @@ import com.example.mathias.findyourfriends.Helpers.ToastMaker;
 import com.example.mathias.findyourfriends.Helpers.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,6 +53,21 @@ public class DatabaseConnector {
         Group group = new Group(groupName, id);
 
         ref.child(id).setValue(group);
+
+        ref.child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Group group = dataSnapshot.getValue(Group.class);
+                joinGroup(group.getID());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //joinGroup(ref.child("id").toString());
     }
 
     public void joinGroup(String id) {
@@ -60,7 +78,7 @@ public class DatabaseConnector {
         Map map = new HashMap();
         map.put("groupID", id);
 
-        ref.child(uid).updateChildren(map);
+        FirebaseDatabase.getInstance().getReference().child("Users").child(uid).updateChildren(map);
     }
 
     public void updateLocation(double lat, double lng) {
